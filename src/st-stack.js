55b5b73f4193dfaa174fa4bf03756a5bc36c3245
user <angelice92@mail.ru>
2021-10-15 21:14:44 +0300

    feat: add st-stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,15 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
+     constructor(){
+         this.stack = [];
+     }
   push(element) {
-
+      this.stack.push(element)
   }
 
   pop() {
-
+   return this.stack.pop() || undefined
   }
 
   peek() {
-
+      return this.stack[this.stack.length - 1] || undefined
   }
 }
+
