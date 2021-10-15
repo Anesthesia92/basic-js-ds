cf0f48875a9f053ed6f0d2a0a9851ee91cdf6b5b
user <angelice92@mail.ru>
2021-10-15 19:09:33 +0300

    feat: binary-search-tree add, has

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,18 +7,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
   constructor() {
-    this.tree = null;
+    this.leaves = null;
   }
   root() {
-    return this.tree
+    return this.leaves
   }
 
   add(data) {
-
+let x = this.leaves
+    if (!x) {
+      x = new Node()
+    } else {
+      while (x) {
+        if (data < x.data) {
+          if (x.left) {
+            x = x.left
+          } else {
+            x.left = new Node(data)
+            break
+          }
+        }
+        if (data > x.data) {
+          if (x.right) {
+            x = x.right
+          } else {
+            x.right = new Node (data)
+            break
+          }
+        }
+      }
+    }
   }
 
   has(data) {
-
+    return !!this.find(data)
   }
 
   find(data) {
